5b34c8616fb793ecd7907a297f4015071c2aba22
Bui The Hoa <buithehoa@gmail.com>
2014-11-17 23:38:17 +0700

    Rename curf to curve.

--- app/controllers/curves_controller.rb
+++ app/controllers/curves_controller.rb
@@ -1,5 +1,5 @@
 class CurvesController < ApplicationController
-  before_action :set_curf, only: [:show, :edit, :update, :destroy]
+  before_action :set_curve, only: [:show, :edit, :update, :destroy]
 
   # GET /curves
   # GET /curves.json
@@ -14,7 +14,7 @@ class CurvesController < ApplicationController
 
   # GET /curves/new
   def new
-    @curf = Curve.new
+    @curve = Curve.new
   end
 
   # GET /curves/1/edit
@@ -24,15 +24,15 @@ class CurvesController < ApplicationController
   # POST /curves
   # POST /curves.json
   def create
-    @curf = Curve.new(curf_params)
+    @curve = Curve.new(curve_params)
 
     respond_to do |format|
-      if @curf.save
-        format.html { redirect_to @curf, notice: 'Curve was successfully created.' }
-        format.json { render :show, status: :created, location: @curf }
+      if @curve.save
+        format.html { redirect_to @curve, notice: 'Curve was successfully created.' }
+        format.json { render :show, status: :created, location: @curve }
       else
         format.html { render :new }
-        format.json { render json: @curf.errors, status: :unprocessable_entity }
+        format.json { render json: @curve.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,12 +41,12 @@ class CurvesController < ApplicationController
   # PATCH/PUT /curves/1.json
   def update
     respond_to do |format|
-      if @curf.update(curf_params)
-        format.html { redirect_to @curf, notice: 'Curve was successfully updated.' }
-        format.json { render :show, status: :ok, location: @curf }
+      if @curve.update(curve_params)
+        format.html { redirect_to @curve, notice: 'Curve was successfully updated.' }
+        format.json { render :show, status: :ok, location: @curve }
       else
         format.html { render :edit }
-        format.json { render json: @curf.errors, status: :unprocessable_entity }
+        format.json { render json: @curve.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,7 +54,7 @@ class CurvesController < ApplicationController
   # DELETE /curves/1
   # DELETE /curves/1.json
   def destroy
-    @curf.destroy
+    @curve.destroy
     respond_to do |format|
       format.html { redirect_to curves_url, notice: 'Curve was successfully destroyed.' }
       format.json { head :no_content }
@@ -63,12 +63,12 @@ class CurvesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_curf
-      @curf = Curve.find(params[:id])
+    def set_curve
+      @curve = Curve.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def curf_params
-      params.require(:curf).permit(:question, :answer, :status)
+    def curve_params
+      params.require(:curve).permit(:question, :answer, :status)
     end
 end
