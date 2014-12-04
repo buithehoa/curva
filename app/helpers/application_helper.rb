c49564f96b1ea294ce88319984d97aecec0becb6
Bui The Hoa <buithehoa@gmail.com>
2014-12-04 21:54:33 +0700

    Highlight the selected menu item.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def selected_item_class(path)
+    recognized_path = Rails.application.routes.recognize_path path
+
+    controller.controller_name == recognized_path[:controller] ? "selected" : ""
+  end
 end
